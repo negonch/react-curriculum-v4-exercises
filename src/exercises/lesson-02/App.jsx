9b5ad6023b5cc4a69a10a563e92c2f8faa2b35c7
negonch <negonch@gmail.com>
2026-04-14 19:07:23 -0700

    Lesson-02 complete

---
+++ src/exercises/lesson-02/App.jsx
@@ -0,0 +1,15 @@
+import SnackHeader from './SnackHeader.jsx';
+import SnackList from './SnackList.jsx';
+import SnackFooter from './SnackFooter.jsx';
+
+function App() {
+  return (
+    <div>
+      <SnackHeader />
+      <SnackList />
+      <SnackFooter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/exercises/lesson-02/SnackHeader.jsx
@@ -0,0 +1,9 @@
+function SnackHeader() {
+  return (
+    <div>
+      <h1>Snack Rankings</h1>
+    </div>
+  );
+}
+
+export default SnackHeader;
